84d44e50fcda5558e83978d3440d109bc2692153
yalr098 <yvesromero1998@gmail.com>
2022-11-29 15:38:39 -0300

    added more robust connection pipeline

--- drone/utils.py
+++ drone/utils.py
@@ -1,10 +1,27 @@
+import errno
+import os
 import time
 from logging import DEBUG, FileHandler, Formatter, Logger, getLogger
+from socket import error
+from typing import Any
 
 import cv2
 import numpy as np
 import pyboof as pb
+import regex as re
+import requests
+from websocket import WebSocket, create_connection
 
+LOG_PATH = os.getenv("LOG_PATH")
+
+CAMERA_ERROR_REPLACE = "[{}] Could not connect to camera. Tried {} time(s)"
+camera_error_matcher = re.compile(".*Could not connect to camera. Tried (?P<tries>\d+) time\(s\)$")
+
+ASK_INFO_ERROR_REPLACE = "Endpoint {} request has failed. Tried {} time(s)"
+ask_info_matcher = re.compile(".*request has failed. Tried (?P<tries>\d+) time\(s\)$")
+
+WEBSOCKET_REFUSED_REPLACE = "Websocket {} connection refused. Tried {} time(s)"
+websocket_refused_matcher = re.compile(".*connection refused. Tried (?P<tries>\d+) time\(s\)$")
 
 def make_logger():
 
@@ -77,3 +94,92 @@ def read_qrcode(detector, logger_, FILENAME, frame, request_id):
         "error": False
     }
     return messages
+
+def read_log_lines():
+
+    lines: list[str] = open('drone.log', 'r').readlines()
+
+    return lines
+
+def check_log_last_line(
+    logger: Logger, 
+    request_string: str,
+    matcher: Any,
+    replace_string: str
+):
+
+    lines: list[str] = read_log_lines()
+
+    last_line: str = lines[-1]
+
+    MATCH = matcher.match(last_line)
+    if MATCH is None:
+        WRITE_MESSAGE = replace_string.format(request_string, 1)
+        logger.error(WRITE_MESSAGE)
+        return
+
+    tries = MATCH.groupdict()["tries"]
+    REPLACE_MESSAGE = replace_string.format(request_string, int(tries) + 1)
+
+    del lines[-1]
+
+    open(LOG_PATH, 'w').writelines(lines)
+    logger.error(REPLACE_MESSAGE)
+
+    return
+
+def check_log_last_line_camera(logger, request_string):
+
+    check_log_last_line(
+        logger, request_string, 
+        camera_error_matcher, 
+        CAMERA_ERROR_REPLACE
+    )
+
+    
+
+def ask_info_to_manager(logger, request_string):
+
+    while True:
+
+        try:
+            response = requests.get(request_string).json()
+
+        except:
+            check_log_last_line(
+                logger, request_string, 
+                ask_info_matcher, 
+                ASK_INFO_ERROR_REPLACE
+            )
+            time.sleep(5)
+            continue
+
+        break
+
+    return response
+
+def get_websocket_connection(
+    logger: Logger
+):
+
+    while True:
+
+        try:
+            ws_string = "ws://{}:{}/capture/ws".format(
+            os.getenv("MANAGER_HOST"), os.getenv("MANAGER_PORT")
+            )
+            ws: WebSocket = create_connection(ws_string)
+            
+        except error as err:
+            if err.errno == errno.ECONNREFUSED:
+                check_log_last_line(
+                    logger, ws_string, 
+                    websocket_refused_matcher, 
+                    WEBSOCKET_REFUSED_REPLACE
+                )
+            time.sleep(5)
+            continue
+
+        break
+    
+    return ws
--- start.py
+++ start.py
@@ -7,35 +7,20 @@ from typing import Any
 import cv2
 import numpy as np
 import pyboof as pb
-import websocket
-from websocket import WebSocket, create_connection
-
-from drone.config import DRONE_ID, FILENAME, VIDEO_URL
-from drone.utils import make_logger, read_qrcode
-
 from dotenv import load_dotenv
 
-env_path = "/home/pi/drone/.env.drone"
-load_dotenv(env_path)
+from drone.config import ENV_PATH, FILENAME
+
+load_dotenv(ENV_PATH)
+
+from websocket import WebSocket
+
+from drone.utils import (ask_info_to_manager, check_log_last_line_camera,
+                         get_websocket_connection, make_logger, read_qrcode)
+
+DRONE_ID = os.getenv("DRONE_ID")
 
 logger_ = make_logger()
-
-try:
-    detector = pb.FactoryFiducial(np.uint8).qrcode()
-except:
-    logger_.error("Erro na geração de detector pyboof")
-    time.sleep(120)
-    exit(1)
-
-state: dict[str, Any] = {
-    "capture_qrcode": False,
-    "connected": False,
-    "transmission_on": False,
-    "video_capture": None
-}
-
-VIDEO_URL = 0
-state["video_capture"] = cv2.VideoCapture(VIDEO_URL)
 
 def frame_reader() -> None:
 
@@ -46,6 +31,7 @@ def frame_reader() -> None:
         ret = state["video_capture"].grab()
         if not ret:
             state["transmission_on"] = False
+            return
 
 def message_exchanges(ws: WebSocket):
 
@@ -57,6 +43,9 @@ def message_exchanges(ws: WebSocket):
 
         try:
             request_id: str = ws.recv()
+
+            if request_id == "connection_test":
+                continue
 
             payload = {
                 "message": None,
@@ -85,25 +74,69 @@ def message_exchanges(ws: WebSocket):
             state["connected"] = False
             return
 
+try:
+    detector = pb.FactoryFiducial(np.uint8).qrcode()
+except:
+    logger_.error("Erro na geração de detector pyboof")
+    time.sleep(120)
+    exit(1)
 
-state["connected"] = True
+state: dict[str, Any] = {
+    "capture_qrcode": False,
+    "connected": False,
+    "transmission_on": False,
+    "video_capture": None
+}
+
+REQUEST_URL = "http://{}:{}/subscribe/get/{}".format(
+    os.getenv("MANAGER_HOST"), os.getenv("MANAGER_PORT"), os.getenv("DRONE_ID")
+)
+
+response = ask_info_to_manager(logger_, REQUEST_URL)
+logger_.info("Endpoint response: {}".format(response))
+try:
+    VIDEO_URL = response["data"]["camera_uri"]
+except:
+    logger_.error("Missing key on endpoint response...")
+    exit(1)
+
+if VIDEO_URL == "0":
+    VIDEO_URL = 0
+
+logger_.info("Trying to connect to camera: {}".format(VIDEO_URL))
+while True:
+
+    try:
+        state["video_capture"] = cv2.VideoCapture(VIDEO_URL)
+    except:
+        last_line_is_error = check_log_last_line_camera(logger_)
+        time.sleep(10)
+        continue
+
+    ret, frame = state["video_capture"].read()
+    if not ret:
+        check_log_last_line_camera(logger_)
+        time.sleep(10)
+        continue
+
+    break
+
+logger_.info("Successfully connected to camera {}".format(VIDEO_URL))
+frame_t = threading.Thread(target=frame_reader)
+frame_t.start()
 
 while True:
 
-    websocket.enableTrace(True)
-    ws: WebSocket = create_connection("ws://{}:{}/capture/ws".format(
-        os.getenv("MANAGER_HOST"), os.getenv("MANAGER_PORT")
-    ))
-
-    frame_t = threading.Thread(target=frame_reader)
+    ws: WebSocket = get_websocket_connection(logger_)
+    state["connected"] = True
     frame_ex = threading.Thread(target=message_exchanges, args=(ws,))
 
-    frame_t.start()
     frame_ex.start()
 
     while state["connected"]:
         time.sleep(0.1)
         if not state["transmission_on"]:
+            state["video_capture"].release()
             state["video_capture"] = cv2.VideoCapture(VIDEO_URL)
             frame_t = threading.Thread(target=frame_reader)
             frame_t.start()
